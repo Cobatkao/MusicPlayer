80dd0972b580e11ea0e80e79ef16d5522dfc9e71
gaohang <isaackao@foxmail.com>
2018-10-26 00:48:34 +0800

    完成歌词滚动，还有一点小bug

--- song.js
+++ song.js
@@ -20,14 +20,16 @@ $(function () {
     parseMusicLyric.call(null, lyric)
   }
 
+  function fillingZero(num) {
+    return num >= 10 ? num + '' : '0' + num
+  }
+
   //请求歌曲url
   function initMusicplayer(url) {
     let audio = document.createElement('audio')
     audio.src = url
     audio.oncanplay = function () {
-      console.log('ready play')
       audio.play()
-      console.log('unable to play')
       $('.disc-container').addClass('playing')
     }
     $('.icon-pause').on('click', function () {
@@ -38,6 +40,32 @@ $(function () {
       audio.play()
       $('.disc-container').addClass('playing')
     })
+
+    setInterval(()=>{
+      let second = audio.currentTime
+      let minute = ~~(second / 60)
+      let leftTime = second - minute * 60
+      //获取当前播放时间
+      let time = `${fillingZero(minute)}:${fillingZero(leftTime)}`
+      console.log(time)
+      let $lines = $('.lines > p')
+      let theVeryLine
+      for(let i = 0; i < $lines.length; i++) {
+        if($lines.eq(i).attr("data-time") < time && $lines.eq(i+1).attr("data-time") > time) {
+          $theVeryLine = $lines.eq(i)
+          break
+        }
+      }
+      //处理歌词滚动
+      if($theVeryLine) {
+        console.log('当前：' + $theVeryLine)
+        $theVeryLine.addClass('active').prev().removeClass('active')
+        let theVeryLyricTop = $theVeryLine.offset().top
+        let linesTop = $('.lines').offset().top
+        let gap = theVeryLyricTop - linesTop - $('.lyric').height() / 3
+        $('.lines').css({'transform': `translateY(-${gap}px)`, 'transition': 'transform .3s'})
+      }
+    },300)
   }
 
   //请求歌曲时间，歌词
