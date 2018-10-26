7ffacd83259a60a54980d3af677f7bdda610e173
gaohang <isaackao@foxmail.com>
2018-10-26 11:10:28 +0800

    tab切换逻辑修改

--- index.js
+++ index.js
@@ -24,4 +24,26 @@ $(function () {
   }, function () {
     console.log('请求失败')
   })
+
+  $('.siteNav').on('click', 'ol.tabItems>li', function(e) {
+    let $li = $(e.currentTarget).addClass('active')
+    $li.siblings().removeClass('active')
+    let index = $li.index()
+    //自定义tab切换事件
+    $li.trigger('tabChange', index)
+    $('.tabContent > li').eq(index).addClass('active')
+    $('.tabContent > li').eq(index).siblings().removeClass('active')
+  })
+  //冒泡监听自定义事件
+  $('.siteNav').on('tabChange', function(e, index) {
+    if(index === 1) {
+      $.get('./hot.json').then(function(response) {
+        console.log(response)
+      })
+    } else if(index === 2) {
+      $.get('./search.json').then(function(response) {
+        console.log(response)
+      }
+    )}
+  })
 })
